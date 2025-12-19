872831e76f26045d049474557fb417fdaba3e33e
LCLP <lukas@lclpnet.work>
2025-12-19 17:45:05 +0100

    migrate to official mappings

--- src/main/java/work/lclpnet/opt_sync/mixin/OptionsMixin.java
+++ src/main/java/work/lclpnet/opt_sync/mixin/OptionsMixin.java
@@ -1,6 +1,6 @@
 package work.lclpnet.opt_sync.mixin;
 
-import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,11 +8,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import work.lclpnet.opt_sync.OptSyncEntrypoint;
 import work.lclpnet.opt_sync.lib.OptionSync;
 
-@Mixin(GameOptions.class)
-public class GameOptionsMixin {
+@Mixin(Options.class)
+public class OptionsMixin {
 
     @Inject(
-            method = "write",
+            method = "save",
             at = @At("TAIL")
     )
     public void mcOptSync$afterConfigSaved(CallbackInfo ci) {
